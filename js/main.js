42c8fcf4888f7674527b938d36b64ffc35225dc9
Nikolay <sebistian@ukr.net>
2021-11-06 23:49:08 +0200

    added pupup and fiexed menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-    $("a.scrollto").click(function () {
-        var elementClick = '#'+$(this).attr("href").split("#")[1]
-        var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 2000);
-        return false;
-    });
+
+    /** Position fixed header **/
+    const positionFixedHeader = () => {
+
+        $(window).scroll(function() {
+            let header = $('.header'),
+                scroll = $(window).scrollTop();
+            if (scroll >= 20) {
+                header.addClass('fixed');
+            } else {
+                header.removeClass('fixed');
+            }
+        })
+    }
+
+    positionFixedHeader();
+
+    /** Show popup **/
+    const showPopup = () => {
+
+        $(document).on('click', '#showPopup', function() {
+            $('.modal').addClass('show');
+        })
+
+    };
+
+    showPopup();
+
+    const hidePopup = () => {
+        $(document).on('click', '.modal__btn, .close', function() {
+            $('.modal').removeClass('show');
+        })
+    };
+    hidePopup();
 });
